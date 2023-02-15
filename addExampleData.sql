1124794c4e4e938ac757d1f14dd6d549e8874551
Karam Chafqane <karam.chafqane@tuni.fi>
2023-02-15 20:15:24 +0200

    Modified script and example data

--- addExampleData.sql
+++ addExampleData.sql
@@ -1,42 +1,71 @@
 
 
 INSERT INTO drones (id, name, brand, model, additional_info)
-VALUES (1, 'Drone A', 'DJI', 'Phantom 4 Pro', 'Has 20 MP camera');
+VALUES (1, 'Drone 1', 'Yamaha', 'DT 50cc', 'Good ol Dt');
 
 INSERT INTO drones (id, name, brand, model, additional_info)
-VALUES (2, 'Drone B', 'Parrot', 'Anafi USA', 'Has thermal imaging');
+VALUES (2, 'Drone 2', 'Bugatti', 'Veyron', 'Kallis');
 
 INSERT INTO drones (id, name, brand, model, additional_info)
-VALUES (3, 'Drone C', 'Yuneec', 'Typhoon H Plus', 'Has 6 rotors');
+VALUES (3, 'Drone 3', 'Lamborghini', 'Gallardo', 'Ihan siisti');
 
+INSERT INTO drones (id, name, brand, model, additional_info)
+VALUES (4, 'Drone 4', 'Porche', '911', 'Perus hieno');
 
-INSERT INTO users (id, name, password, token, additional_info)
-VALUES (1, 'John Doe', '1234', 'abc123', 'Has experience with DJI drones');
-
-INSERT INTO users (id, name, password, token, additional_info)
-VALUES (2, 'Jane Doe', '5678', 'def456', 'New to drone flying');
-
-INSERT INTO users (id, name, password, token, additional_info)
-VALUES (3, 'Bob Smith', 'abcd', 'ghi789', 'Expert in aerial photography');
+INSERT INTO drones (id, name, brand, model, additional_info)
+VALUES (5, 'Drone 5', 'McLaren', 'P1', 'Lemppari');
 
 
 
-INSERT INTO images (id, name, analysis_description, lat, lon, time)
-VALUES (1, 'Image A', 'Agricultural analysis', 37.3352, -121.8811, '2022-01-01 12:00:00');
 
-INSERT INTO images (id, name, analysis_description, lat, lon, time)
-VALUES (2, 'Image B', 'Environmental monitoring', 40.7128, -74.0060, '2022-02-01 12:00:00');
+INSERT INTO users (id, name, password, token, WhenInUse, drones_id)
+VALUES (1, 'Sauli Niinistö', '1234', 'abc123', '2022-01-01 12:00:00', 5);
 
-INSERT INTO images (id, name, analysis_description, lat, lon, time)
-VALUES (3, 'Image C', 'Construction site inspection', 33.9416, -118.4085, '2022-03-01 12:00:00');
+INSERT INTO users (id, name, password, token, WhenInUse, drones_id)
+VALUES (2, 'Tarja Halonen', '5678', 'def456', '2022-02-01 12:00:00', 4);
+
+INSERT INTO users (id, name, password, token, WhenInUse, drones_id)
+VALUES (3, 'Martti Ahtisaari', 'abcd', 'ghi789', '2022-03-01 12:00:00', 1);
+
+INSERT INTO users (id, name, password, token, WhenInUse, drones_id)
+VALUES (4, 'Mauno Koivisto', 'efgh', 'ghi789', '2022-04-01 12:00:00', 3);
+
+INSERT INTO users (id, name, password, token, WhenInUse, drones_id)
+VALUES (5, 'Urho kekkonen', 'ijkl', 'ghi789', '2022-05-01 12:00:00', 2);
 
 
 
-INSERT INTO send_addresses (id, address, token)
-VALUES (1, '123 Main St, San Francisco, CA 94105', 'abc123');
 
-INSERT INTO send_addresses (id, address, token)
-VALUES (2, '456 Park Ave, New York, NY 10022', 'def456');
 
-INSERT INTO send_addresses (id, address, token)
-VALUES (3, '789 Sunset Blvd, Los Angeles, CA 90069', 'ghi789');
+INSERT INTO images (id, name, analysis_description, lat, lon, time, drone_id, user_id)
+VALUES (1, 'Kuva 1', 'Putkivauriot', 37.3352, -121.8811, '2022-01-01 12:00:00', 5, 1);
+
+INSERT INTO images (id, name, analysis_description, lat, lon, time, drone_id, user_id)
+VALUES (2, 'Kuva 2', 'Maisemakuvat', 40.7128, -74.0060, '2022-02-01 12:00:00', 4, 2);
+
+INSERT INTO images (id, name, analysis_description, lat, lon, time, drone_id, user_id)
+VALUES (3, 'Kuva 3', 'Työmaan tutkimus', 33.9416, -118.4085, '2022-03-01 12:00:00', 1, 3);
+
+INSERT INTO images (id, name, analysis_description, lat, lon, time, drone_id, user_id)
+VALUES (4, 'Kuva 4', 'Maisemakuva 2', 33.9416, -118.4085, '2022-04-01 12:00:00', 3, 4);
+
+INSERT INTO images (id, name, analysis_description, lat, lon, time, drone_id, user_id)
+VALUES (5, 'Kuva 5', 'Maisemakuva 3', 33.9416, -118.4085, '2022-05-01 12:00:00', 2, 5);
+
+
+
+
+INSERT INTO send_addresses (id, address, token, images_id)
+VALUES (1, 'Kuntokatu 3, 33520 Tampere', 'abc123', 1);
+
+INSERT INTO send_addresses (id, address, token, images_id)
+VALUES (2, 'Ajurinkatu 20, 70110 Kuopio', 'def456', 4);
+
+INSERT INTO send_addresses (id, address, token, images_id)
+VALUES (3, 'Keltinkatu 4-6, 33530 Tampere', 'ghi789', 3);
+
+INSERT INTO send_addresses (id, address, token, images_id)
+VALUES (4, '1428 Elm street, Springwood, Ohio', 'ghi789', 2);
+
+INSERT INTO send_addresses (id, address, token, images_id)
+VALUES (5, 'Visulahdentie 1, 50180 Mikkeli', 'ghi789', 5);
